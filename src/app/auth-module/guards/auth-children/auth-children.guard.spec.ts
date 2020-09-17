6d94b2d19b7058c8fc7a123e208ffa4352df4ef2
Oleg <oleg@localhost.localdomain>
2020-09-17 14:46:01 +0300

    CROSSPRO-154: fix guards test

--- src/app/auth-module/guards/auth-children/auth-children.guard.spec.ts
+++ src/app/auth-module/guards/auth-children/auth-children.guard.spec.ts
@@ -1,4 +1,11 @@
 import { TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from '../../../../environments/environment';
+import {
+  ENVIRONMENT,
+  IEnvironment
+} from 'shared-kuailian-lib';
 
 import { AuthChildrenGuard } from './auth-children.guard';
 
@@ -6,7 +13,18 @@ describe('AuthGuard', () => {
   let guard: AuthChildrenGuard;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        RouterTestingModule,
+        HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: ENVIRONMENT,
+          useValue: environment as IEnvironment
+        },
+      ]
+    });
     guard = TestBed.inject(AuthChildrenGuard);
   });
 
--- src/app/auth-module/guards/auth/auth.guard.spec.ts
+++ src/app/auth-module/guards/auth/auth.guard.spec.ts
@@ -2,9 +2,14 @@ import { TestBed } from '@angular/core/testing';
 
 import { AuthGuard } from './auth.guard';
 import { RouterTestingModule } from '@angular/router/testing';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { environment } from '../../../../environments/environment';
+import {
+  ENVIRONMENT,
+  IEnvironment
+} from 'shared-kuailian-lib';
 
-fdescribe('AuthGuard', () => {
+describe('AuthGuard', () => {
   let guard: AuthGuard;
 
   beforeEach(() => {
@@ -12,6 +17,12 @@ fdescribe('AuthGuard', () => {
       imports: [
         RouterTestingModule,
         HttpClientTestingModule
+      ],
+      providers: [
+        {
+          provide: ENVIRONMENT,
+          useValue: environment as IEnvironment
+        },
       ]
     });
     guard = TestBed.inject(AuthGuard);
