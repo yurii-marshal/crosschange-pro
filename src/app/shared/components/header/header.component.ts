c83fd18209823ec3f1c436a0aebce1b05390e385
yurii-marshal <ymarshalofficial@gmail.com>
2020-09-22 15:22:47 +0300

    CROSSPRO-460: menu changes init

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -7,8 +7,15 @@ import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class HeaderComponent implements OnInit {
+  menuItems = [
+    {active: false, label: 'header.markets'},
+    {active: false, label: 'header.wallet'},
+    {active: false, label: 'header.exchange'},
+    {active: false, label: 'header.history'},
+  ];
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
