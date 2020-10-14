e573c4453bd87ab61979c6a52030116d8f23146c
Oleg <oleg@localhost.localdomain>
2020-10-14 09:41:32 +0300

    CROSSPRO-319: auth fix

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { BaseComponent } from './shared/components/base/base.component';
 import { CanShowProGuard } from './core/guards/can-show-pro/can-show-pro.guard';
-import { ConnectWebsocketResolver } from './shared/resolvers/connect-websocket.resolver';
 
 export const routes: Routes = [
   {
     path: '',
     component: BaseComponent,
     canActivate: [CanShowProGuard],
-    resolve: {
-      user: ConnectWebsocketResolver
-    },
     children: [
       {
         path: '',
