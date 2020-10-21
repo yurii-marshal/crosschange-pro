779d3056d4937003da391e12b9d0a369ae361359
Oleg <oleg@localhost.localdomain>
2020-10-21 07:36:38 +0300

    CROSSPRO-319: fix chart repaint

--- src/app/home/components/markets/markets.component.ts
+++ src/app/home/components/markets/markets.component.ts
@@ -113,6 +113,10 @@ export class MarketsComponent implements OnInit, OnDestroy {
     });
   }
 
+  widgetTracker(item: IExchangeData): string {
+    return item.exchange_type;
+  }
+
   ngOnDestroy(): void {
     this.onDestroyed$.next();
     this.onDestroyed$.complete();
