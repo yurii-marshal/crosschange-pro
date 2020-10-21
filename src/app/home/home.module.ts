529e308f8865b3a22d5224019d11002a8c69832b
Oleg <oleg@localhost.localdomain>
2020-10-21 18:34:12 +0300

    switchers fix for release

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,16 +5,11 @@ import { HomeComponent } from './components/home/home.component';
 import { SharedModule } from '../shared/shared.module';
 import { MarketsComponent } from './components/markets/markets.component';
 import { AngularMaterialModule } from 'src/app/angular-material.module';
-import { JwtResolver } from 'shared-kuailian-lib';
+
 import { AuthGuard } from '../auth-module/guards/auth/auth.guard';
 import { ReactiveFormsModule } from '@angular/forms';
 
 export const routes: Routes = [
-  {
-    path: 'auth',
-    resolve: { redirect: JwtResolver },
-    children: [],
-  },
   {
     path: '',
     component: HomeComponent,
