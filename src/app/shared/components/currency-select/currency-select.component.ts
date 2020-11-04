4d5de9f7ba70a62c048bed5650a96fe6b63b0e6c
yurii-marshal <ymarshalofficial@gmail.com>
2020-11-04 10:27:52 +0200

    staging: aftermerge

--- src/app/shared/components/currency-select/currency-select.component.ts
+++ src/app/shared/components/currency-select/currency-select.component.ts
@@ -6,7 +6,7 @@ import {
 } from '@angular/core';
 import { BehaviorSubject, Subject } from 'rxjs';
 import { debounceTime, map, take } from 'rxjs/operators';
-import { ControlValueAccessor, FormControl, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { ControlValueAccessor, FormControl, FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
 import { ICurrency } from '../../interfaces/currency.interface';
 import { ExchangeService } from '../../services/exchange.service';
 import { validate } from './CurrencySelectValidator';
@@ -30,13 +30,14 @@ export class CurrencySelectComponent implements OnInit, OnChanges, OnDestroy, Co
   @ViewChild('input') input;
   @ViewChild('mobileInput') mobileInput;
   @Input() disabledCondition;
-  @Output() amountChange: EventEmitter<void> = new EventEmitter<void>();
   public keyUp = new Subject<KeyboardEvent>();
   opened = false;
   currencies: ICurrency[];
   currenciesFiltered$: BehaviorSubject<ICurrency[]> = new BehaviorSubject<ICurrency[]>([]);
   selected$: BehaviorSubject<ICurrency> = new BehaviorSubject<ICurrency>(null);
-  amount: FormControl = new FormControl();
+  amountForm: FormGroup = new FormGroup({
+    amount: new FormControl(''),
+  });
   onDestroy$: Subject<void> = new Subject();
   value: {
     currency: ICurrency,
