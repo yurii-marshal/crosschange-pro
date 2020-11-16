25a75f328af308436a7a1e201775220645a5b237
yurii-marshal <ymarshalofficial@gmail.com>
2020-11-16 20:45:03 +0200

    CROSSPRO-723: styles, dark theme header, table init, service init

--- src/app/home/components/trade/trade.component.ts
+++ src/app/home/components/trade/trade.component.ts
@@ -1,5 +1,12 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/drag-drop';
+import { FormControl } from '@angular/forms';
+import { ActivatedRoute } from '@angular/router';
+import { ITradePairsData, TradeService } from '../../services/trade.service';
+import { combineLatest, Observable } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
+import { defaultPagination } from '../../../shared/constants/pagination.constant';
 
 @Component({
   selector: 'app-trade',
@@ -8,17 +15,44 @@ import { CdkDragDrop, moveItemInArray, transferArrayItem } from '@angular/cdk/dr
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class TradeComponent implements OnInit {
+  isDarkTheme: boolean;
 
   leftContainer: string[] = [];
   centralContainer: string[] = [];
   rightContainer: string[] = [];
 
   isTradeTypeSelectorOpened: boolean;
+  activeLink: string;
+  limit = this.route.snapshot.queryParams.limit || defaultPagination.limit;
 
-  constructor() {
+  searchInputControl = new FormControl();
+
+  pairsDataSource$: Observable<MatTableDataSource<ITradePairsData>>;
+  pairsDisplayedColumns = [
+    'pair',
+    'last_price',
+    'change',
+  ];
+
+  constructor(
+    private route: ActivatedRoute,
+    private tradeService: TradeService,
+  ) {
   }
 
   ngOnInit(): void {
+    this.activeLink = this.route.snapshot.queryParams.tab || 'all';
+
+    this.pairsDataSource$ = combineLatest(
+      [
+        this.searchInputControl.valueChanges
+          .pipe(startWith(''), debounceTime(500), distinctUntilChanged()),
+        this.route.queryParams,
+      ]
+    ).pipe(
+      switchMap(([query, params]) => this.tradeService.getTradePairs(query, params)),
+      map((v) => new MatTableDataSource<ITradePairsData>(v.results)),
+    );
   }
 
   drop(event: CdkDragDrop<string[]>): void {
@@ -32,11 +66,13 @@ export class TradeComponent implements OnInit {
     }
   }
 
-  toggleTradeSelector(): void {
+  toggleTradeSelector(popover): void {
+    popover.toggle();
     this.isTradeTypeSelectorOpened = !this.isTradeTypeSelectorOpened;
   }
 
   playSpotTutorial(): void {
+    this.isDarkTheme = !this.isDarkTheme;
   }
 
 }
--- src/app/home/services/trade.service.ts
+++ src/app/home/services/trade.service.ts
@@ -1,9 +1,103 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
+import { ApiService } from 'shared-kuailian-lib';
+import { Observable, of } from 'rxjs';
+import { HttpParams } from '@angular/common/http';
+import { defaultPagination } from '../../shared/constants/pagination.constant';
+
+export interface ITradePairsData {
+  pair: string;
+  lastPrice: {
+    price: number;
+    exchanged: number;
+  };
+  change: number;
+}
+
+const pairsDataSourceMock = [
+  {
+    pair: 'ETH/AUD',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: -1.5,
+  },
+  {
+    pair: 'ETH/BIDR',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: 3.00,
+  },
+  {
+    pair: 'ETH/BKRW',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: -1.5,
+  },
+  {
+    pair: 'ETH/BTC',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: -1.5,
+  },
+  {
+    pair: 'ETH/BUSD',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: -1.5,
+  },
+  {
+    pair: 'ETH/BIDR',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: 3.00,
+  },
+  {
+    pair: 'ETH/BKRW',
+    lastPrice: {
+      price: 0.003610,
+      exchanged: 56.06,
+    },
+    change: -1.5,
+  },
+];
 
 @Injectable({
   providedIn: 'root'
 })
-export class TradeService {
+export class TradeService extends ApiService {
 
-  constructor() { }
+  constructor(protected injector: Injector) {
+    super(injector);
+  }
+
+  getTradePairs(query: string, params): Observable<{ results: ITradePairsData[], count: number }> {
+    // TODO: uncomment when endpoint is ready
+    // let parameters = new HttpParams();
+    // parameters = parameters
+    //   .set('search', query || '')
+    //   .set('type', params.tab || 'all')
+    //   .set('orderby', params.orderby || 'last')
+    //   .set('offset', params.offset || defaultPagination.offset)
+    //   .set('limit', params.limit || defaultPagination.limit);
+    //
+    // return super.get('trade/pairs', {params: parameters});
+
+    // TODO: remove when endpoint is ready
+    return of({
+      results: query ? pairsDataSourceMock.filter(v => v.pair.toLowerCase().includes(query.toLowerCase())) : pairsDataSourceMock,
+      count: pairsDataSourceMock.length,
+    });
+  }
+
 }
